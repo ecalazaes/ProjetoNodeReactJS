49fe15ed43bcdbc636462f73d373ffe4bb146bea
Erick Calazães <ecalazaes@gmail.com>
2025-06-24 01:15:16 -0300

    POPUP COM REACT

--- frontend/src/pages/plates/page.jsx
+++ frontend/src/pages/plates/page.jsx
@@ -1,20 +1,31 @@
 import plateServices from "../../services/plates.jsx";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import Loading from "../loading/page.jsx";
 import PlateCard from "../../components/plateCard/plateCard.jsx";
+import styles from "./page.module.css"
+import PlatePopup from "../../components/platePopup/platePopup.jsx";
 
-export default function Plates(){
+export default function Plates() {
 
-    const {getAvailablePlates, platesList, plateLoading, refetchPlates} = plateServices()
+    const {getAvailablePlates, platesList, platesLoading, refetchPlates} = plateServices()
+    const [ plateSelected, setPlateSelected ] = useState(null);
 
     useEffect(() => {
-       if (refetchPlates) {
-           getAvailablePlates()
+        if (refetchPlates) {
+            getAvailablePlates()
         }
     }, [refetchPlates]);
 
-    if (plateLoading) {
-        return (<Loading />)
+    const handlePlateSelected = (plate) => {
+        setPlateSelected(plate)
+    }
+
+    const handleClosePopup = () => {
+        setPlateSelected(null)
+    }
+
+    if (platesLoading) {
+        return (<Loading/>)
     }
 
     console.log(platesList)
@@ -23,9 +34,17 @@ export default function Plates(){
         <>
             <div>
                 {platesList.map((plate) => (
-                    <PlateCard plateData={plate} key={plate._id} />
+                    <div key={plate._id} className={styles.cardContainer} onClick={() => {
+                        handlePlateSelected(plate)
+                    }}>
+                        <PlateCard plateData={plate}/>
+                    </div>
                 ))}
             </div>
+
+            {plateSelected && (
+                <PlatePopup plateData={plateSelected} onClose={handleClosePopup}/>
+            )}
         </>
     )
 }
