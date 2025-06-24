812cf7d7babdb868dc4cf54fcd7f9cda6e9b7514
Erick Calazães <ecalazaes@gmail.com>
2025-06-24 02:05:55 -0300

    CRIANDO NOSSO CARRINHO

--- frontend/src/pages/cart/page.jsx
+++ frontend/src/pages/cart/page.jsx
@@ -1,12 +1,50 @@
 import {useCartContext} from "../../contexts/useCartContext.jsx";
+import styles from "./page.module.css";
+import {LuCircleMinus} from "react-icons/lu";
 
-export default function Cart(){
+export default function Cart() {
 
     const {cartItems} = useCartContext()
 
     console.log(cartItems)
 
+    if (!cartItems.length) {
+        return (
+            <div>
+                <h1> Your cart is empty... :/</h1>
+                <button>See our specialities!</button>
+            </div>
+        )
+
+    }
     return (
-        <h1>Cart</h1>
-    )
+        <div className={styles.pageContainer}>
+            <h1>Your items: </h1>
+            <section>
+                <div className={styles.itemListContainer}>
+                    {cartItems.map((item) => (
+                        <div className={styles.itemContainer} key={item._id}>
+                            <img src={item.imgUrl} alt=""/>
+                            <div className={styles.itemContent}>
+                                <h2>{item.name}</h2>
+                                <p>[{String(item.ingredients)}]</p>
+                                <p>{item.description}</p>
+                                <div className={styles.portionsContainer}>
+                                    <p>Portions:</p>
+                                    <p>{item.quantity}</p>
+                                    <div className={styles.portionsBtns}>
+                                        <button>-</button>
+                                        <button>+</button>
+                                    </div>
+                                </div>
+                                <button><LuCircleMinus /> Remove item</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </section>
+
+            <button className={styles.confirmeBtn}>Confirm your order!</button>
+        </div>
+    );
 }
